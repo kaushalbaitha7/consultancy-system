4afbb4d9fffac9005fba76ee6c44b0faa11385be
kaushalbaitha7 <kaushalbaitha74@gmail.com>
2026-05-21 18:33:11 +0530

    fixed render deployment

--- server/server.js
+++ server/server.js
@@ -1,43 +1,57 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const mongoose = require("mongoose");
 
 const cors = require("cors");
 
-require("dotenv").config();
-
 const authRoutes =
-require("./routes/authRoutes");
+    require("./routes/authRoutes");
 
 const app = express();
 
 
-// MIDDLEWARES
+
+// MIDDLEWARE
+
 app.use(cors());
 
 app.use(express.json());
 
 
-// ROUTES
-app.use("/api/auth", authRoutes);
 
+// ROUTES
+
+app.use(
+    "/api/auth",
+    authRoutes
+);
+
+
+
+// TEST ROUTE
 
 app.get("/", (req, res) => {
 
-    res.send("Consultancy Server Running");
+    res.send(
+        "Consultancy Server Running"
+    );
 
 });
 
 
-const PORT =
-process.env.PORT || 5000;
 
+// DATABASE CONNECTION
 
 mongoose.connect(process.env.MONGO_URI)
 
 .then(() => {
 
     console.log("MongoDB Connected");
+
+    const PORT =
+        process.env.PORT || 5000;
 
     app.listen(PORT, () => {
 
@@ -49,8 +63,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 })
 
-.catch((err) => {
+.catch((error) => {
 
-    console.log(err);
+    console.log(error);
 
 });
